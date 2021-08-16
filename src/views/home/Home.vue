c1917d0942ad9bcef61cf1965dd5f45c1c170b38
fanyao <fanyaoyao12138@163.com>
2021-08-16 17:24:44 +0800

    将首页拆分为3个组件

---
+++ src/views/home/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="warpper">
+    <StaticPart />
+    <Nearby />
+  </div>
+  <Docker />>
+</template>
+
+<script>
+import StaticPart from './StaticPart.vue'
+import Nearby from './Nearby.vue'
+import Docker from './Docker.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    StaticPart,
+    Nearby,
+    Docker
+  }
+}
+</script>
+
+<style lang="scss">
+@import '../../style/viriables.scss';
+@import '../../style/mixins.scss';
+
+.warpper {
+  overflow-y: auto;
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: 0.5rem;
+  right: 0;
+  padding: 0 0.18rem 0.01rem 0.18rem;
+}
+</style>
---
+++ src/views/home/StaticPart.vue
@@ -0,0 +1,170 @@
+<template>
+  <div class="position">
+    <span class="iconfont position__icon">&#xe619;</span>
+    北京理工大学国防科技园2号楼10层
+    <span class="iconfont position__notice">&#xe9cf;</span>
+  </div>
+  <div class="search">
+    <span class="iconfont">&#xe728;</span>
+    <span class="search__text">山姆会员商店优惠商品</span>
+  </div>
+  <div class="banner">
+    <img
+      class="banner__image"
+      src="http://www.dell-lee.com/imgs/vue3/banner.jpg"
+    />
+  </div>
+  <div class="icons">
+    <div class="icons__item">
+      <img
+        class="icons__item__image"
+        src="http://www.dell-lee.com/imgs/vue3/超市.png"
+      />
+      <p class="icons__item__desc">超市便利</p>
+    </div>
+    <div class="icons__item">
+      <img
+        class="icons__item__image"
+        src="http://www.dell-lee.com/imgs/vue3/菜市场.png"
+      />
+      <p class="icons__item__desc">菜市场</p>
+    </div>
+    <div class="icons__item">
+      <img
+        class="icons__item__image"
+        src="http://www.dell-lee.com/imgs/vue3/水果店.png"
+      />
+      <p class="icons__item__desc">水果店</p>
+    </div>
+    <div class="icons__item">
+      <img
+        class="icons__item__image"
+        src="http://www.dell-lee.com/imgs/vue3/鲜花.png"
+      />
+      <p class="icons__item__desc">鲜花绿植</p>
+    </div>
+    <div class="icons__item">
+      <img
+        class="icons__item__image"
+        src="http://www.dell-lee.com/imgs/vue3/医药健康.png"
+      />
+      <p class="icons__item__desc">医药健康</p>
+    </div>
+    <div class="icons__item">
+      <img
+        class="icons__item__image"
+        src="http://www.dell-lee.com/imgs/vue3/家居.png"
+      />
+      <p class="icons__item__desc">家居时尚</p>
+    </div>
+    <div class="icons__item">
+      <img
+        class="icons__item__image"
+        src="http://www.dell-lee.com/imgs/vue3/蛋糕.png"
+      />
+      <p class="icons__item__desc">烘焙蛋糕</p>
+    </div>
+    <div class="icons__item">
+      <img
+        class="icons__item__image"
+        src="http://www.dell-lee.com/imgs/vue3/签到.png"
+      />
+      <p class="icons__item__desc">签到</p>
+    </div>
+    <div class="icons__item">
+      <img
+        class="icons__item__image"
+        src="http://www.dell-lee.com/imgs/vue3/大牌免运.png"
+      />
+      <p class="icons__item__desc">大牌免运</p>
+    </div>
+    <div class="icons__item">
+      <img
+        class="icons__item__image"
+        src="http://www.dell-lee.com/imgs/vue3/红包.png"
+      />
+      <p class="icons__item__desc">红包套餐</p>
+    </div>
+  </div>
+  <div class="gap"></div>
+</template>
+
+<style lang="scss">
+@import "../../style/viriables.scss";
+@import "../../style/mixins.scss";
+
+.position {
+  position: relative;
+  padding: 0.16rem 0.24rem 0.16rem 0;
+  line-height: 0.22rem;
+  font-size: 0.16rem;
+  @include ellipsis;
+  color: $content-fontcolor;
+  .position__icon {
+    position: relative;
+    top: 0.01rem;
+    font-size: 0.2rem;
+  }
+  .position__notice {
+    position: absolute;
+    right: 0;
+    top: 0.17rem;
+    font-size: 0.24rem;
+  }
+}
+.search {
+  line-height: 0.32rem;
+  background: #f5f5f5;
+  border-radius: 0.16rem;
+  margin-bottom: 0.12rem;
+  color: #b7b7b7;
+  .iconfont {
+    display: inline-block;
+    padding: 0 0.08rem 0 0.16rem;
+    font-size: 0.16rem;
+  }
+  &__title {
+    font-size: 0.14rem;
+    display: inline-block;
+  }
+}
+.banner {
+  // padding-bottom防止图片抖动
+  height: 0;
+  overflow: hidden;
+  padding-bottom: 25.4%;
+  &__image {
+    width: 100%;
+  }
+}
+.icons {
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 0.16rem;
+  &__item {
+    width: 20%;
+    &__image {
+      display: block;
+      width: 0.4rem;
+      height: 0.4rem;
+      margin: 0 auto;
+    }
+    &__desc {
+      text-align: center;
+      margin: 0.06rem 0 0.16rem 0;
+      color: $content-fontcolor;
+    }
+  }
+}
+.gap {
+  height: 0.1rem;
+  background: $content-bgColor;
+  margin: 0 -0.18rem;
+}
+</style>
+
+<script>
+export default {
+  name: 'StaticPart'
+}
+</script>
